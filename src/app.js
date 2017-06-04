29ad5cea89a75fa22125c5a51db37f650561a1e1
Yash Patel <yashpatel68714@gmail.com>
2017-06-04 21:59:56 +0530

    added google maps

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
-(function(){
-    console.log("in");
-    let a = "test";
-    if(true){
-        console.log("a is" + a);
-    }
-})()
+$(document).ready(function(){
+    $(".header-item").click(function(){
+        var item = $(this).find(" span").text()
+        var index = $(this).index() + 1;
+        console.log("clicked ",item);
+        $('.header-item').removeClass('selected');
+        $(this).addClass("selected");
+        console.log($('.right-section section:nth-child('+index+')'));
+        $('.right-section section').removeClass('active-section');
+        $('.right-section section:nth-child('+index+')').addClass('active-section');
+    }); 
+    console.log("dom ready");
+    // createMap();
+});
+
+function createMap(){
+    var cords = {lat: 23.061847, lng: 72.526301};
+    var map = new google.maps.Map(document.getElementsByClassName("map")[0],{
+        zoom: 15,
+        center: cords
+    });
+
+    var marker = new google.maps.Marker({
+        position: cords,
+        map: map
+    });
+}
